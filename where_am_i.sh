6c872858ec4e49b741c45a0b50626500c67ff818
Clewsy <jadon.clews@gmail.com>
2017-03-10 16:43:58 +0800

    Add files via upload

--- where_am_i.sh
+++ where_am_i.sh
@@ -3,17 +3,19 @@
 BOLD=`tput bold`
 RESET=`tput sgr0`
 
+TEMP_FILE=$(dirname $BASH_SOURCE)/temp	#define the temp file location so that the script will work even if run from a directory without write access
+
 echo
 echo "Pulling data from "ipinfo.io"..."
-curl -s -o temp ipinfo.io
+curl -s -o $TEMP_FILE ipinfo.io
 
-ip=$(cat temp | grep "ip" | head -c-3 | tail -c+10)
-hostname=$(cat temp | grep "hostname" | head -c-3 | tail -c+16)
-city=$(cat temp | grep "city" | head -c-3 | tail -c+12)
-region=$(cat temp | grep "region" | head -c-3 | tail -c+14)
-country=$(cat temp | grep "country" | head -c-3 | tail -c+15)
-loc=$(cat temp | grep "loc" | head -c-3 | tail -c+11)
-org=$(cat temp | grep "org" | head -c-2 | tail -c+11)
+ip=$(cat $TEMP_FILE | grep -m 1 "ip" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+hostname=$(cat $TEMP_FILE | grep "hostname" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+city=$(cat $TEMP_FILE | grep "city" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+region=$(cat $TEMP_FILE | grep "region" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+country=$(cat $TEMP_FILE | grep "country" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+loc=$(cat $TEMP_FILE | grep "loc" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+org=$(cat $TEMP_FILE | grep "org" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 
 echo
 echo "${BOLD}IP:${RESET}------------ ${ip}"
@@ -26,7 +28,7 @@ echo "${BOLD}Organisation:${RESET}-- ${org}"
 echo
 
 
-rm temp
+rm $TEMP_FILE
 
 
 
