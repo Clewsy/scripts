a170cac3fda85f598f4489f2db542c053a06f5e2
Clewsy <jadon.clews@gmail.com>
2017-04-21 17:07:18 +0800

    Add files via upload

--- vpn.sh
+++ vpn.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #I wrote this script to kill then reconnect the vpn (using openvpn).  Useful for when the connection starts acting up.
-#Loop at the end will exit once once the script confirms the apparent city has changed from the current.
-#This loop will be infinite if the vpn is in the same city as the machine.
+#Loop at the end will exit once once the script confirms the apparent ip has changed from the current.
 
 RED="\033[31m"
 GREEN="\033[32m"
@@ -10,6 +9,9 @@ RESET="\033[0m"
 
 #define the vpn config file to be used by openvpn.
 VPN_FILE="/etc/openvpn/Windscribe-Australia.ovpn"
+
+#define a temp file into which ipinfo.io data will be entered and then parsed
+TEMP_FILE=$(dirname $BASH_SOURCE)/temp	#using $dirname of $BASH_SOURCE will create temp in the current working directory
 
 #following if will quit the script if curl is not installed.
 if [ ! $(which curl) ] ; then	#check if curl is not installed
@@ -24,12 +26,22 @@ echo
 echo "Killing any running instances of openvpn"
 sudo killall openvpn
 
-#determine the current city location (without vpn)
-current_city=$(curl -s ipinfo.io | grep city | cut -d ":" -f 2 | cut -d "\"" -f 2)
-new_city=$current_city
+#download current data from ipinfo.io into TEMP_FILE (this is the info without the vpn active)
+curl -s -o $TEMP_FILE ipinfo.io	#-s to supress output while running
+if [ $? != '0' ] ; then		#check if curl exited with a failure
+	echo
+	echo -e "${RED}Error${RESET}: Failed to pull data from "ipinfo.io".  Quitting..."
+	echo
+	exit 0
+fi
 
-#print out the current city (without vpn)
+#parse specific details from the temp file
+current_ip=$(cat $TEMP_FILE | grep -m 1 "ip" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+current_city=$(cat $TEMP_FILE | grep "city" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+
+#print out the current ip and city (without vpn)
 echo
+echo "Current ip:   $current_ip"
 echo "Current city: $current_city"
 
 #connect to the vpn
@@ -46,14 +58,24 @@ if [ $? != '0' ] ; then
 fi
 
 #loop until the vpn is active - determined by a change in the city
-while [ "$new_city" == "$current_city" ]
+new_ip=$current_ip
+while [ "$new_ip" == "$current_ip" ]
 do
-	new_city=$(curl -s ipinfo.io | grep city | cut -d ":" -f 2 | cut -d "\"" -f 2)
+	curl -s -o $TEMP_FILE ipinfo.io
+	if [ $? != '0' ] ; then	#check if curl exited with a failure
+		echo
+		echo -e "${RED}Error${RESET}: Failed to pull data from "ipinfo.io".  Quitting..."
+		echo
+		exit 0
+	fi
+	new_ip=$(cat $TEMP_FILE | grep -m 1  "ip" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 done
+new_city=$(cat $TEMP_FILE | grep "city" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 
-#print out the new apparent city
+#print out the new apparent ip & city
 echo
 echo -e "${GREEN}Connected.${RESET}"
+echo "New ip:   $new_ip"
 echo "New city: $new_city"
 echo
 
