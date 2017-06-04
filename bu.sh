893445fcfcde3ed6e5355158edfa854f200f1a29
Clewsy <jadon.clews@gmail.com>
2017-06-04 07:50:42 +0000

    Ran through shellcheck.net and cleaned some bugs.

--- bu.sh
+++ bu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage="Usage: $basename $0 <backup file list>"
+usage="Usage: $(basename "$0") <backup file list>"
 
 ##Colours
 RED="\033[31m"
@@ -27,7 +27,7 @@ bu_file_list=${1-"${HOME}/bin/bu.list"}	#First argument is the file name of the 
 echo
 echo "Backup file list is \"$bu_file_list\""
 #Validate the backup file list.
-if [ ! -f "$bu_file_list" -o ! -r "$bu_file_list" ]; then	#If bu.list is not (!) a normal file (-f) or (-o) in is not (!) readable (-r)
+if [ ! -f "$bu_file_list" ] || [ ! -r "$bu_file_list" ]; then	#If bu.list is not (!) a normal file (-f) or (-o) in is not (!) readable (-r)
 	echo -e "${RED}Backup file list \"$bu_file_list\" not found, invalid file type or no read access.${RESET}"
 	exit $bad_file_list
 fi
@@ -36,15 +36,15 @@ echo -e "${GREEN}Backup file list \"$bu_file_list\" validated.${RESET}"
 #Validate the backup folder or create if absent.
 echo
 echo "Checking for remote backup directory \"$bu_remote_dir\" on remote backup server \"$bu_server\" (will be created if absent)."
-ssh $bu_server "mkdir -p $bu_remote_dir"	#Connects to the remote server and creates the directory to which bu files will be copied.
-if [ "$?" -ne "0" ]; then			#Checks the exit code from the SSH command.  If not zero, then it failed.
+if ! ssh $bu_server "mkdir -p $bu_remote_dir"	#Connects to the remote server and creates the directory to which bu files will be copied.
+then			#Checks the exit code from the SSH command.  If not zero, then it failed.
 	echo -e "${RED}Failed to create remote directory${RESET}"
 	exit $no_rem_dir
 fi
 echo -e "${GREEN}Remote backup directory \"${bu_remote_dir}\" validated.${RESET}"
 echo
 
-while read bu_file ; do		##Loop to repeat commands for each file name entry in the backup file list ($bu_file_list)
+while read -r bu_file ; do		##Loop to repeat commands for each file name entry in the backup file list ($bu_file_list)
         echo "--------"
 	echo "File/Directory name as listed in \"${bu_file_list}\": $bu_file"		#Filename as read from backup file list
 	full_path="${bu_file/#\~/$HOME}"				#Expanded variable will be treated as a literal string.
@@ -57,9 +57,8 @@ while read bu_file ; do		##Loop to repeat commands for each file name entry in t
 		continue		#Exit the loop and restart for the next file in the list.
 	fi
 	echo "File/Directory \"${full_path}\" exists."
-	echo -e "${GREEN}Backing up \"${full_path}\" to \"${USER}@${bu_server}:${bu_remote_dir}/$(basename ${full_path})\"${RESET}" 
-	scp -r -B "${full_path}" "${USER}@${bu_server}:$bu_remote_dir/"
-	if [ "$?" -ne "0" ]; then                       #Checks the exit code from the SCP command.  If not zero, then it failed.
+	echo -e "${GREEN}Backing up \"${full_path}\" to \"${USER}@${bu_server}:${bu_remote_dir}/$(basename "${full_path}")\"${RESET}" 
+	if ! scp -r -B "${full_path}" "${USER}@${bu_server}:$bu_remote_dir/"; then	#Execute scp and check for error on exit.
 		echo -e "${RED}Failed to copy ${full_path} to remote directory${RESET}"
 		continue
 	fi
