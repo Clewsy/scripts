e66f801dd0996f76cd5f956aca699fb6a8d717bf
Clewsy <jadon.clews@gmail.com>
2017-06-04 08:51:17 +0000

    Ran through shellcheck.net and cleaned some bugs.

--- ytdl.sh
+++ ytdl.sh
@@ -19,20 +19,18 @@ GREEN="\033[32m"
 RESET="\033[0m"
 
 ##Defaults
-#DEFAULT_DOWNLOAD_DIR=/storage/emulated/0/Download/
-DEFAULT_DOWNLOAD_DIR=~/storage/downloads
+DEFAULT_DOWNLOAD_DIR=/storage/emulated/0/Download/
 
 echo "---Entering \"Download\" directory"
-cd ${DEFAULT_DOWNLOAD_DIR}
-if [ $? != '0' ]; then	#check to ensure that the downlaod directory exists and is accessible
+if ! cd ${DEFAULT_DOWNLOAD_DIR}; then	#change dir or quit on failure.
 	echo
-	echo "---error accessing specified download directory.  has "termux-setup-storage" been run?"
+	echo -e "${RED}---error accessing specified download directory.${RESET}  has \"termux-setup-storage\" been run?"
 	exit -1
 fi
 
 echo "current directory= $(pwd)"
 
-if [ -z $(which termux-clipboard-get) ]; then	#if the termux-clipboard-get command is absent then termux-api has not been installed
+if [ -z "$(which termux-clipboard-get)" ]; then	#if the termux-clipboard-get command is absent then termux-api has not been installed
 	echo
 	echo "---termux-api not installed. Installing now:"
 	apt install termux-api
@@ -42,7 +40,7 @@ echo
 echo "---Fetching url from clipboard: (if this hangs, ctrl-c to exit then install termux-api from f-droid)"
 url=$(termux-clipboard-get)
 
-if [ -z $url ]; then
+if [ -z "$url" ]; then
 	echo "---Nothing in clipboard, quitting."
 	exit -1
 fi
@@ -54,17 +52,16 @@ do
 	echo
 	echo "url= $url"
 	echo "---Executing youtube-dl and selecting best video quality"
-	youtube-dl -c -f best $url
-	if [ $? = '0' ]; then
+	if youtube-dl -c -f best "$url"; then	#execute youtube-dl and check for success on exit.
 		echo
 		echo -e "${GREEN}---all done!${RESET}"
-		termux-notification -i "ytdl" -c "Download complete" -t "$(basename $0)"
-		termux-toast "Download complete ($(basename $0))"
+		termux-notification -i "ytdl" -c "Download complete" -t "$(basename "$0")"
+		termux-toast "Download complete ($(basename "$0"))"
 		exit 129
 	else
 		echo
-		echo -e ${RED}---download failed or incomplete.${RESET}
-		read -n1 -p "---press any key to try again."
+		echo -e "${RED}---download failed or incomplete.${RESET}"
+		read -r -n1 -p "---press any key to try again."
 		echo
 	fi
 done
