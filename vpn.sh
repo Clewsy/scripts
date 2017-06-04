32eba23833020859bd10533e2b29659da88b3235
Clewsy <jadon.clews@gmail.com>
2017-06-04 07:34:32 +0000

    Ran through shellcheck.net and cleaned some bugs.

--- vpn.sh
+++ vpn.sh
@@ -11,10 +11,10 @@ RESET="\033[0m"
 VPN_FILE="/etc/openvpn/Windscribe-Australia.ovpn"
 
 #define a temp file into which ipinfo.io data will be entered and then parsed
-TEMP_FILE=$(dirname $BASH_SOURCE)/temp	#using $dirname of $BASH_SOURCE will create temp in the current working directory
+TEMP_FILE=$(dirname "$0")/temp	#using $dirname of $0 will create temp in the current working directory
 
 #following if will quit the script if curl is not installed.
-if [ ! $(which curl) ] ; then	#check if curl is not installed
+if [ ! "$(which curl)" ] ; then	#check if curl is not installed
 	echo
 	echo -e "${RED}Error${RESET}: curl is not installed.  Quitting."
 	echo
@@ -27,18 +27,17 @@ echo "Killing any running instances of openvpn"
 sudo pkill openvpn
 
 #download current data from ipinfo.io into TEMP_FILE (this is the info without the vpn active)
-curl --silent --connect-timeout 5 --max-time 10 --output $TEMP_FILE ipinfo.io
-if [ $? != '0' ] ; then		#check if curl exited with a failure
-	rm $TEMP_FILE
+if ! curl --silent --connect-timeout 5 --max-time 10 --output "$TEMP_FILE" ipinfo.io ; then #execute curl and check if it exited with a failure
+	rm "$TEMP_FILE"
 	echo
-	echo -e "${RED}Error${RESET}: Failed to pull data from "ipinfo.io".  Quitting..."
+	echo -e "${RED}Error${RESET}: Failed to pull data from \"ipinfo.io\".  Quitting..."
 	echo
 	exit -1 
 fi
 
 #parse specific details from the temp file
-current_ip=$(cat $TEMP_FILE | grep -m 1 "ip" | cut -d ":" -f 2 | cut -d "\"" -f 2)
-current_city=$(cat $TEMP_FILE | grep "city" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+current_ip=$(grep -m 1 "ip" "$TEMP_FILE" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+current_city=$(grep "city" "$TEMP_FILE" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 
 #print out the current ip and city (without vpn)
 echo
@@ -49,10 +48,8 @@ echo "Current city: $current_city"
 echo
 echo "Running openvpn using config file at \"$VPN_FILE\""
 echo "(\"sudo pkill openvpn\" to disable)"
-sudo openvpn $VPN_FILE
-#Following if will quit the script if openvpn exits with a failure
-if [ $? != '0' ] ; then
-	rm $TEMP_FILE
+if ! sudo openvpn $VPN_FILE ; then	#Execute openvpn then exit script if it fails
+	rm "$TEMP_FILE"
 	echo
 	echo -e "${RED}Error:${RESET} openvpn failed. Quitting"
 	echo
@@ -63,17 +60,16 @@ fi
 new_ip=$current_ip
 while [ "$new_ip" == "$current_ip" ]
 do
-	curl --silent --connect-timeout 5 --max-time 10 --output $TEMP_FILE ipinfo.io
-	if [ $? != '0' ] ; then	#check if curl exited with a failure
-		rm $TEMP_FILE
+	if ! curl --silent --connect-timeout 5 --max-time 10 --output $TEMP_FILE ipinfo.io ; then	#Execute curl command but exit if it fails
+		rm "$TEMP_FILE"
 		echo
-		echo -e "${RED}Error${RESET}: Failed to pull data from "ipinfo.io".  Quitting..."
+		echo -e "${RED}Error${RESET}: Failed to pull data from \"ipinfo.io\".  Quitting..."
 		echo
 		exit -1
 	fi
-	new_ip=$(cat $TEMP_FILE | grep -m 1  "ip" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+	new_ip=$(grep -m 1 "ip" "$TEMP_FILE" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 done
-new_city=$(cat $TEMP_FILE | grep "city" | cut -d ":" -f 2 | cut -d "\"" -f 2)
+new_city=$(grep "city" "$TEMP_FILE" | cut -d ":" -f 2 | cut -d "\"" -f 2)
 
 #print out the new apparent ip & city
 echo
@@ -82,7 +78,7 @@ echo "New ip:   $new_ip"
 echo "New city: $new_city"
 echo
 
-rm $TEMP_FILE
+rm "$TEMP_FILE"
 
 exit 0
 
