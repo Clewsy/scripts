779986cb3d3286b8c2e9803e4c67deea53d72740
Clewsy <jadon.clews@gmail.com>
2017-06-04 09:11:54 +0000

    Ran through shellcheck.net and cleaned some bugs.

--- grab.sh
+++ grab.sh
@@ -22,12 +22,12 @@ RESET="\033[0m"
 #--append : append data to file in case of interruption
 
 echo
-echo "Usage: $(basename $0) <REMOTE_FILE>"
+echo "Usage: $(basename "$0") <REMOTE_FILE>"
 echo "This usage will copy the specified file from the default server (${DEFAULT_SERVER}) to the current working directory."
 echo
-echo "Alternatively, use \"$(basename $0)\" with no arguments to prompt for remote user, remote server, remote file (source) and local file (target)."
+echo "Alternatively, use \"$(basename "$0")\" with no arguments to prompt for remote user, remote server, remote file (source) and local file (target)."
 
-if [ ! $(which rsync) ] ; then	#check to ensure rsync is installed.  If not, exit.
+if [ ! "$(which rsync)" ] ; then	#check to ensure rsync is installed.  If not, exit.
 	echo
 	echo -e "${RED}Error: rsync is not installed.  Quitting${RESET}"
 	echo
@@ -47,23 +47,23 @@ if [ ! -z "$1" ] ; then	#if an argument exists then it should be the file on the
 	echo "Local file: $loc_file (default)"
 else	#if no argument is entered then request manual input for the remote server/file (source) info and the local file (target) info.
 	echo
-	read -p "Enter remote username: " rem_user        #request username for remote login
-	if [ -z $rem_user ] ; then	#set the remote login username to the default if none specified
+	read -pr "Enter remote username: " rem_user        #request username for remote login
+	if [ -z "$rem_user" ] ; then	#set the remote login username to the default if none specified
 		echo "No remote username set.  Using default"
 		rem_user=$DEFAULT_USER
 	fi
 	echo "Username: $rem_user"
 
 	echo
-	read -p "Enter remote server (default=${DEFAULT_SERVER}): " rem_server   #request server for remote login
-	if [ -z $rem_server ] ; then	#set the remote server to the default if none specified
+	read -pr "Enter remote server (default=${DEFAULT_SERVER}): " rem_server   #request server for remote login
+	if [ -z "$rem_server" ] ; then	#set the remote server to the default if none specified
 		echo "No remote server name set.  Using default."
 		rem_server=$DEFAULT_SERVER
 	fi
 	echo "Server: $rem_server"
 
 	echo
-	read -p "Enter remote directory/file (source): " rem_file #request source file including location on remote server
+	read -pr "Enter remote directory/file (source): " rem_file #request source file including location on remote server
 	if [ -z "$rem_file" ] ; then
 		echo "No source file entered.  Using default."
 		rem_file=$DEFAULT_REMOTE_FILE
@@ -71,7 +71,7 @@ else	#if no argument is entered then request manual input for the remote server/
 	echo "Remote (source) file: $rem_file"
 
 	echo
-	read -p "Enter local directory/file (target): " loc_file #request target file including location on local machine
+	read -pr "Enter local directory/file (target): " loc_file #request target file including location on local machine
 	if [ -z "$loc_file" ] ; then
 		echo "No local file set.  Using default."
 		loc_file=$DEFAULT_LOCAL_FILE
@@ -80,12 +80,10 @@ else	#if no argument is entered then request manual input for the remote server/
 fi
 
 echo
-echo "Command: rsync -rvhz --protect-args --progress --append $rem_user@$rem_server:"$rem_file" "$loc_file""
+echo "Command: rsync -rvhz --protect-args --progress --append $rem_user@$rem_server:\"$rem_file\" \"$loc_file\""
 
 echo
-rsync -rvhz --protect-args --progress --append $rem_user@$rem_server:"$rem_file" "$loc_file"
-
-if [ $? == '0' ] ; then
+if rsync -rvhz --protect-args --progress --append $rem_user@$rem_server:"$rem_file" "$loc_file"; then
 	echo
 	echo -e "${GREEN}Success.${RESET}"
 	echo
