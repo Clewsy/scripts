1005a14bac3e96dadd05b0f4ce53fbd77ed767ca
Clewsy <jadon.clews@gmail.com>
2017-07-14 09:19:19 +0000

    Updated to use rsync.  Will default to scp if rsync is not present.

--- bu.sh
+++ bu.sh
@@ -3,72 +3,106 @@
 usage="Usage: $(basename "$0") <backup file list>"
 
 ##Colours
-RED="\033[31m"
-GREEN="\033[32m"
+RED="\033[02;31m"
+ORANGE="\033[02;33m"
+GREEN="\033[02;32m"
+BLUE="\033[01;34m"
 RESET="\033[0m"
 
 #Exit codes
-too_many_args=1
-bad_file_list=2
-no_rem_dir=3	#ssh command to create remote directory failed
+TOO_MANY_ARGS=1	#incorrest usage
+BAD_FILE_LIST=2	#specified or default file list not readable
+NO_SERVICE=3    #neither rsync nor scp are installed
+NO_REM_DIR=4	#ssh command to create remote directory failed
 
-bu_server="b4t.site"
-bu_remote_dir="$HOME/file_cache/$HOSTNAME"
+#Configurable settings
+BU_USER="b4t"
+BU_SERVER="b4t.site"
+BU_REMOTE_DIR="$HOME/file_cache/$HOSTNAME"
+BU_FILE_LIST=${1-"${HOME}/bin/bu.list"}		#First argument is the file name of the list of files to be backed up.
+						#If argument not provided, set default. Syntax: parameter=${parameter-default}
+
 
 #Check valid usage.
 if [ $# -gt 1 ]; then	#Check than no more than one argument is provided.
 	echo -e "${RED}Too many arguments${RESET}"
 	echo "$usage"
-	exit $too_many_args
+	exit $TOO_MANY_ARGS
 fi
 
-bu_file_list=${1-"${HOME}/bin/bu.list"}	#First argument is the file name of the list of files to be backed up.
-				#If argument not provided, set default. Syntax: parameter=${parameter-default}
-echo
-echo "Backup file list is \"$bu_file_list\""
+
 #Validate the backup file list.
-if [ ! -f "$bu_file_list" ] || [ ! -r "$bu_file_list" ]; then	#If bu.list is not (!) a normal file (-f) or (-o) in is not (!) readable (-r)
-	echo -e "${RED}Backup file list \"$bu_file_list\" not found, invalid file type or no read access.${RESET}"
-	exit $bad_file_list
+echo
+echo "Backup file list is \"$BU_FILE_LIST\""
+if [ ! -f "$BU_FILE_LIST" ] || [ ! -r "$BU_FILE_LIST" ]; then	#If bu.list is not (!) a normal file (-f) or (||) in is not (!) readable (-r)
+	echo -e "${RED}Backup file list \"$BU_FILE_LIST\" not found, invalid file type or no read access.${RESET}"
+	exit $BAD_FILE_LIST
 fi
-echo -e "${GREEN}Backup file list \"$bu_file_list\" validated.${RESET}"
+echo -e "${GREEN}Backup file list \"$BU_FILE_LIST\" validated.${RESET}"
+
+
+#Verify if rsync is installed.  If not, verify scp is installed.
+echo
+echo -e "Checking for rsync (preferred) or scp:"
+if ! which rsync >> /dev/null ; then		#If rsync not installed (send to /dev/null to suppress stdout)
+	echo
+	echo -e "${ORANGE}Warning: rsync not installed.${RESET}  Defaulting to scp for backup operations"
+	if ! which scp >> /dev/null; then	#if scp not installed (send to /dev/null to suppress stdout)
+		echo -e "${RED}Error: scp is also not installed.${RESET}  Quitting."
+		exit $NO_SERVICE
+	fi
+	RSYNC_INSTALLED="FALSE"	#Flag false so scp is used for copy operations
+else
+	echo -e "${GREEN}Confirmation:${RESET} rsync installed."
+	RSYNC_INSTALLED="TRUE"	#Flag true so rsync is used for copy operations
+fi
+
 
 #Validate the backup folder or create if absent.
 echo
-echo "Checking for remote backup directory \"$bu_remote_dir\" on remote backup server \"$bu_server\" (will be created if absent)."
-if ! ssh $bu_server "mkdir -p $bu_remote_dir"	#Connects to the remote server and creates the directory to which bu files will be copied.
+echo "Checking for remote backup directory \"$BU_REMOTE_DIR\" on remote backup server \"$BU_SERVER\" (will be created if absent)."
+if ! ssh $BU_SERVER "mkdir -p $BU_REMOTE_DIR"	#Connects to the remote server and creates the directory to which bu files will be copied.
 then			#Checks the exit code from the SSH command.  If not zero, then it failed.
 	echo -e "${RED}Failed to create remote directory${RESET}"
-	exit $no_rem_dir
+	exit $NO_REM_DIR
 fi
-echo -e "${GREEN}Remote backup directory \"${bu_remote_dir}\" validated.${RESET}"
-echo
+echo -e "${GREEN}Remote backup directory \"${BU_REMOTE_DIR}\" validated.${RESET}"
 
-while read -r bu_file ; do		##Loop to repeat commands for each file name entry in the backup file list ($bu_file_list)
+
+#Loop through the file list, validate file names and run backup command
+echo
+while read -r BU_FILE ; do		##Loop to repeat commands for each file name entry in the backup file list ($BU_FILE_LIST)
         echo "--------"
-	echo "File/Directory name as listed in \"${bu_file_list}\": $bu_file"		#Filename as read from backup file list
-	full_path="${bu_file/#\~/$HOME}"				#Expanded variable will be treated as a literal string.
-	full_path="${full_path/\$HOME/$HOME}"				#These two commands evaluate first "~" and then "$HOME"
+	#Expand the filename
+	echo "File/Directory name as listed in \"${BU_FILE_LIST}\": $BU_FILE"		#Filename as read from backup file list
+	FULL_PATH="${BU_FILE/#\~/$HOME}"				#Expanded variable will be treated as a literal string.
+	FULL_PATH="${FULL_PATH/\$HOME/$HOME}"				#These two commands evaluate first "~" and then "$HOME"
 									#then substitute either for the actual variable $HOME
 									#Syntax: ${variable/string_match/replacement}
-	echo "Expanded filename: \"${full_path}\""
-	if [ ! -e "${full_path}" ]; then	#If file doesn't exist
-		echo -e "${RED}File/Directory \"${full_path}\" does not exist.  Skipping...${RESET}"
+	echo "Expanded filename: \"${FULL_PATH}\""
+	#Check file exists
+	if [ ! -e "${FULL_PATH}" ]; then	#If file doesn't exist
+		echo -e "${RED}File/Directory \"${FULL_PATH}\" does not exist.  Skipping...${RESET}"
 		continue		#Exit the loop and restart for the next file in the list.
 	fi
-	echo "File/Directory \"${full_path}\" exists."
-	echo -e "${GREEN}Backing up \"${full_path}\" to \"${USER}@${bu_server}:${bu_remote_dir}/$(basename "${full_path}")\"${RESET}" 
-	#about to run scp;
-		#-r for recursively copying files within dirs
-		#-B for batch mode to avoid multilpe password requests
-		#-p to preserve modification times, access times, and modes
-	if ! scp -r -B -p "${full_path}" "${USER}@${bu_server}:$bu_remote_dir/"; then	#Execute scp and check for error on exit.
-		echo -e "${RED}Failed to copy ${full_path} to remote directory${RESET}"
-		continue
+	echo "File/Directory \"${FULL_PATH}\" exists."
+
+	#Run copy command (rsync or scp)
+	echo -e "${BLUE}Backing up \"${FULL_PATH}\" to \"${BU_USER}@${BU_SERVER}:${BU_REMOTE_DIR}/${RESET}"
+	if [ "$RSYNC_INSTALLED" == "TRUE" ]; then	#Use rsync (preferred, dir structure will be retained within backup dir)
+		if ! rsync -r -R -v --progress "${FULL_PATH}" "${BU_USER}@${BU_SERVER}:${BU_REMOTE_DIR}/"; then	#Execute rsync, check for exit error
+			echo -e "${RED}Failed to copy ${FULL_PATH} to remote directory.${RESET}"
+			continue
+		fi
+	else						#Use scp (dir structure will not be retained)
+		if ! scp -r -B "${FULL_PATH}" "${BU_USER}@${BU_SERVER}:${BU_REMOTE_DIR}/"; then	#Execute scp, check for exit error
+			echo -e "${RED}Failed to copy ${FULL_PATH} to remote directory.${RESET}"
+			continue
+		fi
 	fi
 	echo -e "${GREEN}Success.${RESET}"
 
-done < "$bu_file_list"		##File read by the while loop which includes a list of files to be backed up.
+done < "$BU_FILE_LIST"		##File read by the while loop which includes a list of files to be backed up.
 
 echo
 echo "Backup complete"
