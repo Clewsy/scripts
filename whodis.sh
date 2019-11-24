8ec03704902cd673c21c205801d64151a6056b5b
clewsy <jadon@clews.pro>
2019-11-25 06:25:22 +1100

    Formatting.

--- whodis.sh
+++ whodis.sh
@@ -2,30 +2,35 @@
 
 ## This script grabs the /tmp/dhcp.leases from a defined host (likely a router) and outputs the prettified content to screen.
 
-ROUTER="root@192.168.1.1"				#Define the user and hostame of the router.
+# Define the user and hostame of the router.
+ROUTER="root@192.168.1.1"
 
-ssh $ROUTER "cat /tmp/dhcp.leases" > TEMPFILE_1		#Connect to the router and cat the dhcp.leases file.  Store to local temp file.
+# Connect to the router and cat the dhcp.leases file.  Store to local temp file.
+ssh $ROUTER "cat /tmp/dhcp.leases" > TEMPFILE_1
 
-while read row; do					# Loop through each row in the temp file.
+# Loop through each row in the temp file.
+while read -r row; do
 
-DATE=$(date --date=@"$(echo ${row} | cut -d " " -f 1)" +%Y-%m-%d)
-TIME=$(date --date=@"$(echo ${row} | cut -d " " -f 1)" +%T)
-MAC=$(echo ${row} | cut -d " " -f 2)
-IP=$(echo ${row} | cut -d " " -f 3)
-HOSTNAME=$(echo ${row} | cut -d " " -f 4)
+	DATE=$(date --date=@"$(echo "${row}" | cut -d " " -f 1)" +%Y-%m-%d)	# Parse date
+	TIME=$(date --date=@"$(echo "${row}" | cut -d " " -f 1)" +%T)		# Parse time
+	MAC=$(echo "${row}" | cut -d " " -f 2)					# Parse MAC address
+	IP=$(echo "${row}" | cut -d " " -f 3)					# Parse IP address
+	NAME=$(echo "${row}" | cut -d " " -f 4)					# Parse hostname
 
-echo "${DATE} ${TIME} ${MAC} ${IP} ${HOSTNAME}" >> TEMPFILE_2	# Write to second temp file.
+	echo "${DATE} ${TIME} ${MAC} ${IP} ${NAME}" >> TEMPFILE_2		# Write to second temp file.
 
-done <TEMPFILE_1
+done < TEMPFILE_1
 
+# Append column headings to the top of the temp file ("1i" puts the text at row 1)
+sed -i "1i DATE TIME MAC IP HOSTNAME" TEMPFILE_2
 
-sed -i "1i DATE TIME MAC IP HOSTNAME" TEMPFILE_2	# Append column headings to the top of the temp file ("1i" puts the text at row 1)
-
+# Print the file to stdout with column for nice formatting.
 echo
-column -t TEMPFILE_2					# Print the file to stdout with column for nice formatting.
+column -t TEMPFILE_2
 echo
 
-rm TEMPFILE_1 TEMPFILE_2				# Clean up, delete the temp files.
+# Clean up, delete the temp files.
+rm TEMPFILE_1 TEMPFILE_2
 
 exit 0
 
