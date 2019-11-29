9c45edea2f4566cac8ecd1e1fc84015ad33c6a26
clewsy <jadon@clews.pro>
2019-11-29 15:28:44 +1100

    Added verbosity and help options.  Moved temp files to /tmp.  Tidied comments.

--- ball.sh
+++ ball.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-##This script will taka a remote host or list of hosts, then run the backup script "bu.sh" for each.
-##That said, the "COMMAND" variable can be set to anything you want to run on multiple systems.
+## This script will taka a remote host or list of hosts, then run the backup script "bu.sh" for each.
+## That said, the "COMMAND" variable can be set to anything you want to run on multiple systems.
 
-COMMAND="~/bin/bu.sh"	#The command to run on the remote host/s.  Note using "~" avoids expanding on the client side.
+COMMAND="~/bin/bu.sh"	## The command to run on the remote host/s.  Note using "~" avoids expanding on the client side.
 
 ##########Colours.
 RED="\033[02;31m"
@@ -12,82 +12,104 @@ BOLD="\033[01;37m"
 RESET="\033[0m"
 
 ##########Exit codes
-SUCCESS=0	#Toight.
-BAD_USAGE=1	#Incorrect usage.
+SUCCESS=0	## Toight.
+BAD_USAGE=1	## Incorrect usage.
 
 #########Usage
 USAGE="
-Usage: $(basename "$0") [host/list]
+Usage: $(basename "$0")[option] [host/list]
 Where [host/list] is either:
 	host	-	a specific remote system on which command \"${COMMAND}\" will be run.
 	list	-	a text list of remote systems on which command \"${COMMAND}\" will be run.
 
 If no argument is provided, a list file of the name my_hosts.list will be used (if present).
+
+Options:	-v	Verbose output.
+		-h	Show this help.
 "
 
+VERBOSITY=""	## Define the default verbosity (i.e. none).  Can be changed with otion -v.
+
+##########Interpret options
+while getopts 'fvh' OPTION; do				## Call getopts to identify selected options and set corresponding flags.
+	case "$OPTION" in
+		v)	VERBOSITY="-v" ;;		## -v activates verbose mode by iadding the -v flag to the bu.sh command.
+		h)	echo -e "${USAGE}"		## -h option just prints the usage then quits.
+			exit ${SUCCESS}			## Exit successfully.
+			;;
+		?)
+			echo -e "Invalid option/s."
+			echo -e "${USAGE}"		## Invalid option, show usage.
+			exit ${BAD_USAGE}		## Exit.
+			;;
+	esac
+done
+shift $((OPTIND -1))	## This ensures only non-option arguments are considered arguments when referencing $#, #* and $n.
+
+
 ##########Check correct usage.
-if [ $# -gt 1 ]; then			##Check if more than one argument was entered.
-	echo -e "Too many arguments."	##If so, show usage and exit.
+if [ $# -gt 1 ]; then			## Check if more than one argument was entered.
+	echo -e "Too many arguments."	## If so, show usage and exit.
 	echo -e "${USAGE}"
 	exit ${BAD_USAGE}
 fi
 
 ##########Define the argument (user input or default).
-ARGUMENT=${1-"$(dirname "$0")/my_hosts.list"}	#First argument is the file name of the list of remote systems.
-						#If argument not provided, set default (by_hosts.list in same dir as script).
-						#Syntax: parameter=${parameter-default}.
+ARGUMENT=${1-"$(dirname "$0")/my_hosts.list"}	## First argument is the file name of the list of remote systems.
+						## If argument not provided, set default (by_hosts.list in same dir as script).
+						## Syntax: parameter=${parameter-default}.
 
 ##########Define the temp files.
-TEMP_BALL_SUMMARY="$(dirname "$0")/temp_ball_summary"			#Define the temp summary file location.
-if [ -e "${TEMP_BALL_SUMMARY}" ]; then rm "${TEMP_BALL_SUMMARY}"; fi	#If it exists, delete the temp file (in case script failed previously).
+TEMP_BALL_SUMMARY="/tmp/temp_ball_summary"				## Define the temp summary file location.
+if [ -e "${TEMP_BALL_SUMMARY}" ]; then rm "${TEMP_BALL_SUMMARY}"; fi	## If it exists, delete the temp file (in case script failed previously).
 
-TEMP_REM_SYS_LIST="$(dirname "$0")/temp_rem_sys_list"			#Define a working system list.
-if [ -e "${TEMP_REM_SYS_LIST}" ]; then rm "${TEMP_REM_SYS_LIST}"; fi	#If it exists, delete the temporary file (in case script failed previously).
+TEMP_REM_SYS_LIST="/tmp/temp_rem_sys_list"				## Define a working system list.
+if [ -e "${TEMP_REM_SYS_LIST}" ]; then rm "${TEMP_REM_SYS_LIST}"; fi	## If it exists, delete the temporary file (in case script failed previously).
 
 
 ##########Validate the argument and thus define the remote host/s.
-#Arg specifies a remote host.
-if [ ! -f "${ARGUMENT}" ] || [ ! -r "${ARGUMENT}" ]; then	#If argument is not (!) a normal file (-f) or (||) in is not (!) readable (-r).
-	echo -e "\nRemote system is \"${ARGUMENT}\"."		#Then assume provided argument is a single host (either [host] or [user@host]).
-	echo "${ARGUMENT}" > "${TEMP_REM_SYS_LIST}"		#Create the temp list file which will just contain the single entry.
-#Arg specifies a list of remote hosts.
+## Arg specifies a remote host.
+if [ ! -f "${ARGUMENT}" ] || [ ! -r "${ARGUMENT}" ]; then	## If argument is not (!) a normal file (-f) or (||) in is not (!) readable (-r).
+	echo -e "\nRemote system is \"${ARGUMENT}\"."		## Then assume provided argument is a single host (either [host] or [user@host]).
+	echo "${ARGUMENT}" > "${TEMP_REM_SYS_LIST}"		## Create the temp list file which will just contain the single entry.
+## Arg specifies a list of remote hosts.
 else
-	echo -e "\nRemote system list is \"${ARGUMENT}\"."			#Tell the user the list looks okay.
-	while read -r LINE ; do							#Iterate for every line in the system list.
-		STRIPPED_LINE="$(echo "${LINE}" | cut -d "#" -f 1)"		#Strip the content of the line after (and including) the first '#'.
-		if [ "${STRIPPED_LINE}" ]; then					#If there is anything left in the string (i.e. if entire row is NOT a comment).
-	  		echo "${STRIPPED_LINE}" >> "${TEMP_REM_SYS_LIST}"	#Then copy the stripped line to the temp file.
+	echo -e "\nRemote system list is \"${ARGUMENT}\"."			## Tell the user the list looks okay.
+	while read -r LINE ; do							## Iterate for every line in the system list.
+		STRIPPED_LINE="$(echo "${LINE}" | cut -d "#" -f 1)"		## Strip the content of the line after (and including) the first '#'.
+		if [ "${STRIPPED_LINE}" ]; then					## If there is anything left in the string (i.e. if entire row is NOT a comment).
+	  		echo "${STRIPPED_LINE}" >> "${TEMP_REM_SYS_LIST}"	## Then copy the stripped line to the temp file.
 		fi
 	done < "${ARGUMENT}"
 fi
 
-#Loop through the remote system list.
+## Loop through the remote system list.
 echo -e "\n--------------------------------------------------------------------"
-while read -r REM_SYS <&2; do	##Loop to repeat commands for each file name entry in the backup file list ($BU_FILE_LIST)
-				##<&2 needed as descriptor for nested while read loops (while read loop within called script)
+while read -r REM_SYS <&2; do	## Loop to repeat commands for each file name entry in the backup file list ($BU_FILE_LIST)
+				## <&2 needed as descriptor for nested while read loops (while read loop within called script)
 
 	echo -e "Attempting to run on \"${REM_SYS}\""
 
-	##Switch to set the tab spacing depending on the length of the hostname (makes the ouput summary pretty).
+	## Switch to set the tab spacing depending on the length of the hostname (makes the ouput summary pretty).
 	case ${#REM_SYS} in
-		[1-6])		COLUMN_SPACER="\t\t";;	##1-6 characters
-		[7-9] | 1[0-4])	COLUMN_SPACER="\t";;	##7-14 characters
-		*)		COLUMN_SPACER=""	##>14 characters
+		[1-6])		COLUMN_SPACER="\t\t";;	## 1-6 characters
+		[7-9] | 1[0-4])	COLUMN_SPACER="\t";;	## 7-14 characters
+		*)		COLUMN_SPACER=""	## >14 characters
 	esac
 
-	if ! ssh -t "${REM_SYS}" "${COMMAND}"; then					#Attempt to connect via ssh and run the backup script "bu.sh"
-		echo -E "${REM_SYS}${COLUMN_SPACER} ${RED}Failure.${RESET}" >> "${TEMP_BALL_SUMMARY}"	#Record if the above fails for the current host.
-		continue								# then try the next host in the list.
+	if ! ssh -t "${REM_SYS}" "${COMMAND} ${VERBOSITY}"; then					## Attempt to connect via ssh and run the backup script "bu.sh"
+		echo -E "${REM_SYS}${COLUMN_SPACER} ${RED}Failure.${RESET}" >> "${TEMP_BALL_SUMMARY}"	## Record if the above fails for the current host.
+		continue										##  then try the next host in the list.
 	else
-		echo -E "${REM_SYS}${COLUMN_SPACER} ${GREEN}Success.${RESET}" >> "${TEMP_BALL_SUMMARY}"	#If the above succeeds, record the success.
-											#Note a "success" means the ssh session was created and exited.
-											# gracefully.  Failures with the called script are not checcked.
+		echo -E "${REM_SYS}${COLUMN_SPACER} ${GREEN}Success.${RESET}" >> "${TEMP_BALL_SUMMARY}"	## If the above succeeds, record the success.
+													## Note a "success" means the ssh session was created and exited.
+													##  gracefully.  Failures with the called script are not checcked.
 	fi
 	echo -e "--------------------------------------------------------------------"
 
-done 2< "${TEMP_REM_SYS_LIST}"		##File read by the while loop which includes a list of files to be backed up.
+done 2< "${TEMP_REM_SYS_LIST}"		## File read by the while loop which includes a list of files to be backed up.
 
-#Print out in a pretty format a table indicating the success or failure for each host in the list.
+## Print out in a pretty format a table indicating the success or failure for each host in the list.
 echo -e "${BOLD}\n╔════════Summary:════════╗${RESET}"
 while read -r RESULT ; do
 	echo -e "${BOLD}║${RESET}${RESULT}${BOLD}║${RESET}"
@@ -95,6 +117,6 @@ done < "${TEMP_BALL_SUMMARY}"
 echo -e "${BOLD}╚════════════════════════╝${RESET}"
 echo
 
-rm "${TEMP_BALL_SUMMARY}" "${TEMP_REM_SYS_LIST}"	#Delete the temporary files.
+rm "${TEMP_BALL_SUMMARY}" "${TEMP_REM_SYS_LIST}"	## Delete the temporary files.
 
 exit ${SUCCESS}
