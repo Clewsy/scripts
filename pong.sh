e251ec19f0b6b432c2b45dc2fd65435c48e91ee3
clewsy <jadon@clews.pro>
2020-12-12 19:19:45 +1100

    Overhauled.  Replaced echo with printf.  More verbose output.  Improved host parsing

--- pong.sh
+++ pong.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#: Title:	: pong
+#: Author	: clewsy (clewsy.pro)
+#: Description	: Quickly sequentially ping a list of hosts then provide a summary of the ping results.
+#: Options	: -v - Verbose mode (show additional output).
+#:		  -h - Help (print usage information).
 
-## Colours
+## Colours and formatting
 RED="\033[02;31m"
 GREEN="\033[02;32m"
+BLUE="\033[01;34m"
 BOLD="\033[01;37m"
 DIM="\\033[2m"
 RESET="\033[0m"
+TABLE_WIDTH=31
+FILL_DASH_f () { for ((i=1; i<=$1; i++)); do printf "%s" "-"; done; } ## Print '-' a specified number of times.
+FILL_DOUBLE_f () { for ((i=1; i<=$1; i++)); do printf "%s" "═"; done; } ## Print '═' a specified number of times.
 
 ## Exit codes
 SUCCESS=0		## I guess it worked?
@@ -17,7 +26,7 @@ DEST="/dev/null"	## Default destination for command output.  I.e. don't display 
 
 ## Usage
 USAGE="
-$(basename "$0") runs a quick ping for a list of hosts to determine availibility on the network.
+$(basename "$0") Sequentially ping a list of hosts to determine availibility on the network.
 
 Usage: ${BOLD}$(basename "$0") <option> [host-list]${RESET}
 
@@ -25,16 +34,15 @@ Where [host-list] is a text list of remote systems to test for availability.
 
 Entering no filename for a host-list defaults to ./my_hosts.list
 
-Example hosts format for the host-list:
-hostname		${DIM}## As defined by the locat network hosts list or a dns lookup.${RESET}
-user@hostname		${DIM}## The user is discarded.${RESET}
-ip-address		${DIM}## IPv4 or IPv6.${RESET}
-user@ip-address		${DIM}## The user is discarded.${RESET}
-
 Options:
-	-v	- 	Verbose - print additional info to stdout.
-	-h	-	Print this usage and exit.
+-v : Verbose - print additional info to stdout.
+-h : Print this usage and exit.
 
+Example hosts format for the host-list:${BOLD}
+hostname		${DIM}## As defined by the local network hosts list or a dns lookup.${RESET}${BOLD}
+user@hostname		${DIM}## The user is discarded.${RESET}${BOLD}
+ip-address		${DIM}## IPv4 or IPv6.${RESET}${BOLD}
+user@ip-address		${DIM}## The user is discarded.${RESET}${BOLD}
 "
 
 ############################## Input/syntax/error/dependency checking.
@@ -42,103 +50,96 @@ Options:
 ## Interpret options
 while getopts 'vh' OPTION; do			## Call getopts to identify selected options and set corresponding flags.
 	case "$OPTION" in
-		v)	DEST="/dev/stdout" ;;	## -v activates verbose mode by sending output to /dev/stdout (instead of /dev/null).
-		h)	echo -e "$USAGE"	## -h option just prints the usage then quits.
-			exit ${SUCCESS} ;;	## Exit successfully.
-		?)	echo -e "$USAGE"	## Invalid option, show usage.
-			exit ${BAD_USAGE} ;;	## Exit.
+		v)	DEST="/dev/stdout" ;;		## -v activates verbose mode by directing output to /dev/stdout.
+		h)	printf "%b" "${USAGE}\n"	## -h option just prints the usage then quits.
+			exit ${SUCCESS} ;;		## Exit successfully.
+		?)	printf "%b" "${USAGE}\n"	## Invalid option, show usage.
+			exit ${BAD_USAGE} ;;		## Exit with error code.
 	esac
 done
 shift $((OPTIND -1))	## This ensures only non-option arguments are considered arguments when referencing $#, #* and $n.
 
 ## Check correct usage
-if [ $# -gt 1 ]; then			## Check if more than one argument was entered.
-	echo -e "Too many arguments."	## If so, show usage and exit.
-	echo -e "${USAGE}"
+if [ $# -gt 1 ]; then								## Check if more than one argument was entered.
+	printf "%b" "${RED}Error.${RESET} Too many arguments.\n ${USAGE}\n"	## If so, show usage and exit.
 	exit ${BAD_USAGE}
 fi
 
 ## Validate the list of remote systems.
 REM_SYS_LIST=${1-"$(dirname "$0")/my_hosts.list"}	## First argument is the file name of the list of remote systems.
 							## If argument not provided, set default (my_hosts.list).
-echo -e "\nRemote system list is \"${REM_SYS_LIST}\"." > ${DEST}
-if [ ! -f "${REM_SYS_LIST}" ] || [ ! -r "${REM_SYS_LIST}" ]; then	## If ball.list is not (!) a normal file (-f) or (||) in is not (!) readable (-r)
-	echo -e "\n${RED}Failure:${RESET} Remote system list \"${REM_SYS_LIST}\" not found, invalid file type or no read access.\n"	## Output error message
-	exit ${BAD_LIST_FILE}														## Exit
+printf "%b" "\nRemote system list is \"${REM_SYS_LIST}\".\n" > ${DEST}
+if [ ! -f "${REM_SYS_LIST}" ] || [ ! -r "${REM_SYS_LIST}" ]; then	## Ensure system list is normal (-f) and readable (-r).
+	printf "%b" "\n${RED}Error:${RESET} Remote system list \"${BLUE}${REM_SYS_LIST}${RESET}\" not found, invalid file type or no read access.\n\n"
+	exit ${BAD_LIST_FILE}
 fi
-echo -e "\n${GREEN}Confirmation: ${RESET}Remote system list \"${REM_SYS_LIST}\" validated." > ${DEST}	## Tell user the file list looks okay.
+printf "%b" "\n${GREEN}Success: ${RESET}Remote system list \"${BLUE}${REM_SYS_LIST}${RESET}\" validated.\n" > ${DEST}
+printf "%b" "Raw host list:\n${BLUE}$(cat "${REM_SYS_LIST}")${RESET}\n" > ${DEST}
 
 ## Verify if ping is installed.
-echo -e "\nChecking for ping:" > ${DEST}
+printf "%b" "\nChecking for ping:\n" > ${DEST}
 if ! command -v ping > ${DEST}					#If rsync not installed
-then	echo -e "\n${RED}Failure:${RESET} ping not installed.\n"
+then	printf "%b" "${RED}Error:${RESET} ping not installed.\n\n"
 	exit ${NO_PING}
-else	echo -e "\n${GREEN}Confirmation:${RESET} ping installed." > ${DEST}
+else	printf "%b" "${GREEN}Success:${RESET} ping installed.\n" > ${DEST}
 fi
 
 ############################## Main script functions.
 
 ## Define temporary  output summary file.
-echo -e "\nCreating temp summary file." > ${DEST}
-TEMP_SUMMARY_FILE="/tmp/pong_summary"					## Define the temp file location so that the script will work even if run from a directory without write access
-echo -e "${TEMP_SUMMARY_FILE}" > ${DEST}
-if [ -e "${TEMP_SUMMARY_FILE}" ]; then rm "${TEMP_SUMMARY_FILE}"; fi	## If it exists, delete the temporary file (in case script failed previously before deleting it).
+printf "%b" "\nCreating temp summary file: " > ${DEST}
+TEMP_SUMMARY_FILE="/tmp/pong_summary"
+> "${TEMP_SUMMARY_FILE}"
+printf "%b" "${BLUE}${TEMP_SUMMARY_FILE}${RESET}\n" > ${DEST}
 
 ## Define temporary remote system list.
-echo -e "\nCreating temp host list file." > ${DEST}
-TEMP_REM_SYS_LIST="/tmp/pong_temp_rem_sys_list"				## Create the temporary rem_sys_list file.
-echo -e "${TEMP_REM_SYS_LIST}" > ${DEST}
-if [ -e "${TEMP_REM_SYS_LIST}" ]; then rm "${TEMP_REM_SYS_LIST}"; fi	## If it exists, delete it.
+printf "%b" "\nCreating temp host list file: " > ${DEST}
+TEMP_REM_SYS_LIST="/tmp/pong_temp_rem_sys_list"	
+> "${TEMP_REM_SYS_LIST}"
+printf "%b" "${BLUE}${TEMP_REM_SYS_LIST}${RESET}\n" > ${DEST}
 
-## Create a working system list from the original file list but with #comments stripped.
-while read -r LINE ; do							## Iterate for every line in the system list.
-	STRIPPED_LINE="$(echo "${LINE}" | cut -d "#" -f 1)"		## Strip the content of the line after (and including) the first '#'.
-	if [ "${STRIPPED_LINE}" ] ; then				## If there is anything left in the string (i.e. if entire row is NOT a comment)
-	  	echo "${STRIPPED_LINE}" >> "${TEMP_REM_SYS_LIST}"	## Then copy the stripped line to the temp file.
-	fi
+
+## Strip any comments fromn the host list file.
+printf "%b" "\nStripping comments and users from host list file.\n" > ${DEST}
+while read -r LINE ; do								## Iterate for every line in the system list.
+	STRIPPED_LINE="$(printf "%s" "${LINE}" | cut -d '#' -f 1)"		## Strip after (and including) the first '#'.
+	STRIPPED_LINE="$(printf "%s" "${STRIPPED_LINE}" | cut -d "@" -f 2)"	## Strip before (and including) the first '@'.
+	## If there is anything left hen copy the stripped line to the temp file.
+	if [ "${STRIPPED_LINE}" ]; then printf "%b" "${STRIPPED_LINE}\n" >> "${TEMP_REM_SYS_LIST}"; fi
 done < "${REM_SYS_LIST}"
+printf "%b" "Host list with comments and users stripped:\n${BLUE}$(cat ${TEMP_REM_SYS_LIST})${RESET}\n" > ${DEST}
 
 ## Loop through the remote system list.
 TALLY=0	## Initialise the tally of successful pings.
-echo -en "\n────────Pinging────────" > ${DEST}
+printf "%b" "\n────────Pinging────────" > ${DEST}
 while read -r REM_SYS; do	## Loop to repeat commands for each file name entry in the backup file list ($BU_FILE_LIST)
 
-	REM_HOST=$(echo "${REM_SYS}" | cut -d "@" -f 2)	## Strip the "user@" from the current entry in the hosts file.
+	printf "%b" "\nPinging ${REM_SYS}.." > ${DEST}	## Print current ping - keep the user updated on progress or stall point.
+	printf "%b" "."					## Always print a period for every ping, verbose or standard mode.
 
-	(( NUM_BUFF=23-${#REM_HOST} ))			## Set the padding size based on the number of characters in the hostname.
-	COLUMN_SPACER=""
-	for (( i=1; i<NUM_BUFF; i++ ))
-	do
-		COLUMN_SPACER="${COLUMN_SPACER}-"
-	done
-
-	echo -en "\nPinging ${REM_HOST}.." > ${DEST}	## Print current ping - keep the user updated on progress or stall point.
-	echo -en "."					## Always print a period for every ping, verbose or standard mode.
-
-	if ! ping -4 -c 1 -W 2 "${REM_HOST}" >> /dev/null; then	## Attempt to ping the current host machine.  Ping once (-c 1), wait for 1 second max (-w 1).
-		echo "${GREEN}Ping${RESET} ${REM_HOST}${DIM}${COLUMN_SPACER}${RESET}${RED}Miss${RESET}" >> "$TEMP_SUMMARY_FILE"		## Record failure.
+	if ! ping -4 -c 1 -W 2 "${REM_SYS}" 2> ${DEST} 1> /dev/null; then	## Attempt to ping the current host machine.  Ping once (-c 1), wait for 1 second max (-w 1).
+		printf "%b" "${GREEN}Ping${RESET} ${REM_SYS}${DIM}$(FILL_DASH_f $((TABLE_WIDTH-${#REM_SYS}-11)))${RESET}${RED}Miss${RESET}\n" >> "$TEMP_SUMMARY_FILE"		## Record failure.
 	else
-		echo "${GREEN}Ping${RESET} ${REM_HOST}${DIM}${COLUMN_SPACER}${RESET}${GREEN}Pong${RESET}" >> "$TEMP_SUMMARY_FILE"	## Record success.
+		printf "%b" "${GREEN}Ping${RESET} ${REM_SYS}${DIM}$(FILL_DASH_f $((TABLE_WIDTH-${#REM_SYS}-11)))${RESET}${GREEN}Pong${RESET}\n" >> "$TEMP_SUMMARY_FILE"		## Record failure.
 		(( TALLY++ ))
 	fi
 
 done < "${TEMP_REM_SYS_LIST}"		## File read by the while loop which includes a list of files to be backed up.
-echo -en "\n───────────────────────" > ${DEST}
+printf "%b" "\n───────────────────────\n" > ${DEST}
 
 ## Print out in a pretty format a table indicating the success or failure of pinging each host in the list.
-echo -en "\n\nPing attempts complete.  Printing summary:" > ${DEST}
-echo -e "\n\n${BOLD}╔═════Summary:══════════════════╗${RESET}"
+printf "%b" "\nPing attempts complete.  Printing summary:\n" > ${DEST}
+printf "%b" "\n${BOLD}╔═Summary:$(FILL_DOUBLE_f $((TABLE_WIDTH-11)))╗${RESET}\n"
 while read -r RESULT ; do
-	echo -e "${BOLD}║${RESET}${RESULT}${BOLD}║${RESET}"
+	printf "%b" "${BOLD}║${RESET}${RESULT}${BOLD}║${RESET}\n"
 done < "${TEMP_SUMMARY_FILE}"
-echo -e "${BOLD}╠═══════════════════════════════╣${RESET}"
-echo -e "${BOLD}║ ${TALLY}${RESET} out of ${BOLD}$(wc -l ${TEMP_REM_SYS_LIST} | cut -d " " -f 1)${RESET} hosts online.\t${BOLD}║${RESET}"
-echo -e "${BOLD}╚═══════════════════════════════╝${RESET}\n"
+printf "%b" "${BOLD}╠$(FILL_DOUBLE_f $((TABLE_WIDTH-2)))╣${RESET}\n"
+printf "%b%03d%b%03d%b%$((TABLE_WIDTH-15))b" "${BOLD}║ " "${TALLY}" "${RESET} out of ${BOLD}" "$(wc -l ${TEMP_REM_SYS_LIST} | cut -d ' ' -f 1)" "${RESET} hosts online." "${BOLD}║${RESET}\n"
+printf "%b" "${BOLD}╚$(FILL_DOUBLE_f $((TABLE_WIDTH-2)))╝${RESET}\n\n"
 
 ## Finish up.
-echo -e "Deleting temp files.\n" > ${DEST}
-rm "${TEMP_SUMMARY_FILE}"	## Delete the temporary summary file.
-rm "${TEMP_REM_SYS_LIST}"	## Delete the temporary system list file.
+printf "%b" "Deleting temp files.\n\n" > ${DEST}
+rm "${TEMP_SUMMARY_FILE}" "${TEMP_REM_SYS_LIST}"
 
-echo -e "Script completed ${GREEN}successfully${RESET}.\n" > ${DEST}
+printf "%b" "Script completed ${GREEN}successfully${RESET}.\n\n" > ${DEST}
 exit ${SUCCESS}
